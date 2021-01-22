8438fd3dd57f377095c6659df3f8c5ca3fc4fc70
Deifa <Deifa@users.noreply.github.com>
2021-01-22 23:23:48 +0100

    feature commit

--- src/container/ReusableComponents.js
+++ src/container/ReusableComponents.js
@@ -1,10 +1,13 @@
 import Personaldetails from '../components/Personaldetails';
+import Projectholder from '../components/Projectholder';
+import React from 'react';
 import './ReusableComponents.css';
 
 const ReusableComponents = () => {
     return (
         <div className="ReusableComponents">
            <Personaldetails />
+           <Projectholder />
         </div>
     );
 }
