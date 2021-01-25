4b9563ea1a36ae19ffd4381ba1944f52802c1cee
Deifa <Deifa@users.noreply.github.com>
2021-01-25 03:42:56 +0100

    feature commit

--- src/components/Blogposts.js
+++ src/components/Blogposts.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './Blogposts.css';
 
 const Blogposts = () => {
     return (
         <div className="blogposts">
             <div className="blogpost-grid">
-                <div className="blogpost-title">
-                    <h2>How to organise your CSS</h2>
+                <div className="category">
+                    <h2>Blog</h2>
                 </div>
                 <div className="blogpost-thumbnail">
                     {/* blogpost thumbnail goes here */}
+                </div>
+                <div className="blogpost-title">
+                    <h2>How to organise your CSS</h2>
                 </div>
                 <div className="blogpost-summary">
                     <p>In this article I tell the story about Proin 
@@ -18,6 +22,9 @@ const Blogposts = () => {
                         Donec aliquam est dui, vel vestibulum diam 
                         sollicitudin id. Quisque feugiat malesuada 
                         molestie.</p>
+                </div>
+                <div className="blogpost-host">
+                    <a href="#">Dev.to</a>
                 </div>
             </div>
         </div>
